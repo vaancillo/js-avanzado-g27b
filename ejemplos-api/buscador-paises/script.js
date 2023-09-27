27a3055a47d9df210664a99fbd20271de6e5594c
vancillo <yaredbane@gmail.com>
2023-09-26 21:27:51 -0600

    rendering of countries

--- ejemplos-api/buscador-paises/script.js
+++ ejemplos-api/buscador-paises/script.js
@@ -2,22 +2,32 @@ const searchInput = document.getElementById("search-input")
 const searchButton = document.getElementById("search-button")
 const countryList = document.getElementById("country-list")
 
-function searchCountries(country) {
+const searchCountries = async (country) => {
     fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
     .then(data => {
-        countryList.innerHTML = ""
+        countryList.innerHTML = ''
         data.forEach(country => {
-            const countryDiv = document.createElement("div")
-            countryDiv.className = "country"
-            countryDiv.innerHTML = `
-                <img src="${country.flags.svg}" alt="${country.name}" class="country-flag">
-                <h3 class="country-name">${country.name}</h3>
-                <p class="country-population">${country.population}</p>
-                <p class="country-region">${country.region}</p>
-                <p class="country-capital">${country.capital}</p>
+            const countryCard = document.createElement("div")
+            countryCard.classList.add('country-card')
+            const currencies = Object.keys(country.currencies)
+            const name_currencies = currencies.map(key => country.currencies[key].name).join()
+            const symbol_currencies = currencies.map(key => country.currencies[key].symbol).join()
+            const languages = Object.values(country.languages)
+            const list_languages = languages.map(language => `<p>${language}</p>`).join()
+            console.log(languages)
+            countryCard.className = "country"
+            countryCard.innerHTML = `
+            <img src="${country.flags.png}" alt="${country.name}" class="${country.flags.alt}">
+            <h3 class="flex align-center country-name ">${country.name.common}</h3>
+            <h3 class="flex align-center country-name">${country.name.official}</h3>
+            <p class="country-capital">Capital: ${country.capital}</p>
+            <p class="country-region">Región: ${country.region}</p>
+            <p class="country-currencies">Subregión:Currencies: ${name_currencies}, ${symbol_currencies}</p>
+            <p class="country-languages">Languages: ${languages}</p>
+            <p class="country-population">Poblacion: ${country.population}</p>
             `
-            countryList.appendChild(countryDiv)
+            countryList.appendChild(countryCard)
         })
     })
     .catch(error => {
